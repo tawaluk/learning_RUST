ee8ffab0b55045dc8d1871b396b905fbea9e9c64
tawaluk <tawaluk@yandex.ru>
2025-02-27 23:35:48 +0300

    feat: create srs/bin and 3 files: main, comments, type_var

---
+++ src/bin/comments.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+    // rintln!("Hello, all!");
+
+    println!("Hello, Rust!"); // i am a comment println!("Hello, Rust!");
+
+    /*
+    println!("Hello, Comment!");
+    println!("Hello, Comment!");
+    println!("Hello, Comment!");
+    */ println!("Hello, NotComment!");
+    
+    /* I am a comment */ println!("Hello, NotComment!"); /* I am a comment */
+}
